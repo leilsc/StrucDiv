b12d1cb2d90c89c27665c65c10ad26c2a0d2aa57
reinhardfurrer <reinhard.furrer@math.uzh.ch>
2022-07-19 14:24:22 +0200

    help data

--- R/data.R
+++ R/data.R
@@ -19,7 +19,10 @@
 #' 
 #' For further details, see \url{https://lpdaac.usgs.gov/products/mod13q1v006/}
 #' and \url{https://earthengine.google.com/}
-#'
+#' @examples
+#' # extract a tiny portion (10x30 pixels) in the top left:
+#' ndvi <- raster::raster(ndvi)  # should not be necessary!
+#' tl_ndvi <- raster::crop(ndvi, extent(ndvi, 1, 10, 1, 30))
 "ndvi"
 
 
@@ -46,5 +49,12 @@
 #' 
 #' For further details, see \url{https://lpdaac.usgs.gov/products/mod13q1v006/}
 #' and \url{https://earthengine.google.com/}
-#'
+#' @examples
+#' # This dataset is essentially constructed via:
+#' nGrayLevels <- 15
+#' require(raster)
+#' ndvi <- raster(StrucDiv2::ndvi)
+#' ndvi15 <- cut(ndvi, breaks=seq(minValue(ndvi), maxValue(ndvi), len=nGrayLevels + 1), 
+#'               include.lowest=TRUE, right=FALSE)
+#' 
 "ndvi.15gl"
